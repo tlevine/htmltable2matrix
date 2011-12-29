45ea5899f333025bedee40ac2a92e822799acf1b
Thomas Levine <thomas.levine@gmail.com>
2011-12-29 00:14:46 -0500

    row-by-row tests

---
+++ fixtures/row1.py
@@ -0,0 +1,62 @@
+days="""<form>
+<table>
+<tbody><tr>
+<td align="center" style="color:#ff0000"><img src="/apps/SSAdmin/views/images/downArrow.gif"></td>
+<td style="color:#ff0000; font-size: 14px;">Select Day<br>of Travel</td>
+</tr>
+</tbody></table><select name="selFrequency" onchange="window.location='/coachSS/index.asp?action=RouteReload&amp;order=&amp;sitePageName=%2Fshortline%2Fss.details.asp&amp;dayFilter='+this.value;"><option value="">
+    			Show All
+    		</option>
+<option value="">Show All</option>
+<option value="All">Daily</option>
+<option value="Weekdays">Weekdays</option>
+<option value="Mo">Monday</option>
+<option value="Tu">Tuesday</option>
+<option value="We">Wednesday</option>
+<option value="Th">Thursday</option>
+<option value="Fr">Friday</option>
+<option value="Sa">Saturday</option>
+<option value="Su">Sunday</option></select></form>
+    		&nbsp;
+    	"""
+
+fromstop1="""
+<table cellspacing="0" cellpadding="0" border="0">
+<tbody><tr>
+<td align="left" valign="top"><!--mp_trans_disable_start--><img src="/apps/ssadmin/actions/RenderStopName_ah.asp?title=Cornell%2C%20North%20Campus%2C%20CC%20Lot" alt="Cornell, North Campus, CC Lot" title="Cornell, North Campus, CC Lot"><!--mp_trans_disable_end--></td>
+</tr>
+</tbody></table>
+"""
+
+fromstop2="""
+<table cellspacing="0" cellpadding="0" border="0">
+<tbody><tr>
+<td align="left" valign="top"><!--mp_trans_disable_start--><img src="/apps/ssadmin/actions/RenderStopName_ah.asp?title=Cornell%2C%20West%20Campus%2C%20Baker's%20Flagpole" alt="Cornell, West Campus, Baker's Flagpole" title="Cornell, West Campus, Baker's Flagpole"><!--mp_trans_disable_end--></td>
+</tr>
+</tbody></table>
+"""
+
+fromstop3="""
+<table cellspacing="0" cellpadding="0" border="0">
+<tbody><tr>
+<td align="left" valign="top"><!--mp_trans_disable_start--><img src="/apps/ssadmin/actions/RenderStopName_ah.asp?title=Cornell%2C%20Theater%20Arts%20Ctr." alt="Cornell, Theater Arts Ctr." title="Cornell, Theater Arts Ctr."><!--mp_trans_disable_end--></td>
+</tr>
+</tbody></table>
+"""
+fromstop4="""
+<table cellspacing="0" cellpadding="0" border="0">
+<tbody><tr>
+<td align="left" valign="top"><!--mp_trans_disable_start--><img src="/apps/ssadmin/actions/RenderStopName_ah.asp?title=State%20Street%2C%20Terminal" alt="State Street, Terminal" title="State Street, Terminal"><!--mp_trans_disable_end--></td>
+</tr>
+</tbody></table>
+"""
+arrow=""
+
+tostop="""
+<table cellspacing="0" cellpadding="0" border="0">
+<tbody><tr>
+<td align="left" valign="top"><!--mp_trans_disable_start--><img src="/apps/ssadmin/actions/RenderStopName_ah.asp?title=Port%20Authority%20Bus%20Terminal" alt="Port Authority Bus Terminal" title="Port Authority Bus Terminal"><!--mp_trans_disable_end--></td>
+</tr>
+</tbody></table>
+"""
+route="""<img src="/apps/SSAdmin/views/images/routeTrip.jpg" alt="Route/Trip" title="Route/Trip" width="20" height="110">"""
--- tests.py
+++ tests.py
@@ -1,42 +1,68 @@
 import unittest
 from htmltable2matrix import htmltable2matrix
-from lxml.html import fromstring
-import fixtures.row1
-import fixtures.row2
-import fixtures.bodyrows
+from lxml.html import fromstring,tostring
+from fixtures import row0,row1,bodyrows
 
 def get_table():
   return fromstring(open('fixtures/table.html','r').read())
 
-class Test_lxml(unittest.TestCase):
-
+class TestLXML(unittest.TestCase):
   def setUp(self):
     self.table_xml=get_table()
     self.table_matrix=htmltable2matrix(self.table_xml)
 
-  def no_side_effects(self):
-    self.assertEqual(self.table_xml,get_table())
+  def test_no_side_effects(self):
+    observed=tostring(self.table_xml)
+    expected=tostring(get_table())
+    self.assertEqual(observed,expected)
 
-  def row_count(self):
+  def test_row_count(self):
     self.assertEqual(len(self.table_matrix),14)
 
-  def row1(self):
+  def row0(self):
     expected=[]
     self.assertEqual(self.table_matrix[0],expected)
 
-  def row2(self):
+  def row1(self):
     expected=[
-      row2.days
-    , row2.fromstop1
-    , row2.fromstop2
-    , row2.fromstop3
-    , row2.fromstop4
-    , row2.arrow
-    , row2.tostop
-    , row2.route
+      row1.days
+    , row1.fromstop1
+    , row1.fromstop2
+    , row1.fromstop3
+    , row1.fromstop4
+    , row1.arrow
+    , row1.tostop
+    , row1.route
     ]
-
     self.assertEqual(self.table_matrix[1],expected)
+
+  def test_row1_col0(self):
+    expected=row1.days
+    self.assertEqual(self.table_matrix[1][0],expected)
+
+  def test_row1_col1(self):
+    expected=row1.fromstop1
+    self.assertEqual(self.table_matrix[1][1],expected)
+
+  def test_row1_col2(self):
+    expected=row1.fromstop2
+    self.assertEqual(self.table_matrix[1][2],expected)
+
+  def test_row1_col3(self):
+    expected=row1.fromstop3
+    self.assertEqual(self.table_matrix[1][3],expected)
+
+  def test_row1_col4(self):
+    expected=row1.fromstop4
+    self.assertEqual(self.table_matrix[1][4],expected)
+
+  def test_row1_col5(self):
+    expected=row1.arrow
+    self.assertEqual(self.table_matrix[1][5],expected)
+
+  def test_row1_col6(self):
+    expected=row1.route
+    self.assertEqual(self.table_matrix[1][6],expected)
 
   def bodyrows(self):
     expected=[]
