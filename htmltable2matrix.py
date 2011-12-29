96943edc433faee5da97141cf1488cf639dcbbd1
Thomas Levine <thomas.levine@gmail.com>
2011-12-29 00:22:41 -0500

    aoeu

--- htmltable2matrix.py
+++ htmltable2matrix.py
@@ -1,4 +1,8 @@
+from lxml.html import tostring
 
+def text_content(xml):
+  "Contents of an lxml node"
+  return ''.join(map(tostring,xml.xpath('*'))) 
 
 def htmltable2matrix(tablehtml):
   """Takes an lxml tree whose current node is the table of interest"""
@@ -15,7 +19,8 @@ def htmltable2matrix(tablehtml):
         repeats=1
 
       for r in range(repeats):
-        tablematrix_row.append(td.text)
+        tablematrix_row.append(text_content(td))
+
     tablematrix.append(tablematrix_row)
     del(tds)
   return tablematrix
--- tests.py
+++ tests.py
@@ -36,33 +36,29 @@ class TestLXML(unittest.TestCase):
     ]
     self.assertEqual(self.table_matrix[1],expected)
 
+  def abstract_test_col(self,colnum,expected):
+    self.assertEqual(self.table_matrix[1][colnum],expected)
+
   def test_row1_col0(self):
-    expected=row1.days
-    self.assertEqual(self.table_matrix[1][0],expected)
+    self.abstract_test_col(0,row1.days)
 
   def test_row1_col1(self):
-    expected=row1.fromstop1
-    self.assertEqual(self.table_matrix[1][1],expected)
+    self.abstract_test_col(1,row1.fromstop1)
 
   def test_row1_col2(self):
-    expected=row1.fromstop2
-    self.assertEqual(self.table_matrix[1][2],expected)
+    self.abstract_test_col(2,row1.fromstop2)
 
   def test_row1_col3(self):
-    expected=row1.fromstop3
-    self.assertEqual(self.table_matrix[1][3],expected)
+    self.abstract_test_col(3,row1.fromstop3)
 
   def test_row1_col4(self):
-    expected=row1.fromstop4
-    self.assertEqual(self.table_matrix[1][4],expected)
+    self.abstract_test_col(4,row1.fromstop4)
 
   def test_row1_col5(self):
     expected=row1.arrow
-    self.assertEqual(self.table_matrix[1][5],expected)
 
   def test_row1_col6(self):
     expected=row1.route
-    self.assertEqual(self.table_matrix[1][6],expected)
 
   def bodyrows(self):
     expected=[]
